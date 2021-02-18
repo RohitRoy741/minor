a3251829c461f82d2b4c420cec93c4dbaac8a170
Salamander <rohitroy741760@gmail.com>
2021-02-18 13:42:09 +0530

    Added templated

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Rohit',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'July 14, 2000',
+    },
+    {
+        'author': 'Salamander',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'July 14, 2000',
+    },
+    {
+        'author': 'Anshu',
+        'title': 'Blog Post 3',
+        'content': 'Third Post content',
+        'date_posted': 'July 14, 2000',
+    }
+]
+
+context = {'posts': posts}
+
 
 def home(request):
-	return HttpResponse('<h1>Welcome to Home page</h1>')
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
