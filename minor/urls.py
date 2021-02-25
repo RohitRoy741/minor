9e1f076b377170e6bf20caa1abd15dc62e4fa715
Salamander <rohitroy741760@gmail.com>
2021-02-25 17:23:01 +0530

    Changed Settings

--- minor/urls.py
+++ minor/urls.py
@@ -31,6 +31,4 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
