86105716e5be54604598d0bb63952505dbb38cce
Salamander <rohitroy741760@gmail.com>
2021-03-23 12:35:56 +0530

    Added Postgress DB

--- minor/settings.py
+++ minor/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'minor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'network',
+        'USER': 'Rohit@mujnet',
+        'PASSWORD': 'Alohmora@741760',
+        'HOST': 'mujnet.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
