761e73a0f9f51a26756e63a60f6104b5e0e1a91c
Salamander <rohitroy741760@gmail.com>
2021-03-26 11:44:15 +0530

    Enabled Postgress Database

--- minor/urls.py
+++ minor/urls.py
@@ -23,8 +23,8 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
-    #path('login/', user_views.CustomLogin.as_view(template_name='users/login.html'), name="login"),
+    #path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('login/', user_views.CustomLogin.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('facelogin/', user_views.TwoFactorAuth, name='twofactorauth'),
     path('facelogin/authenticate/', user_views.postFaceLogin, name='postfacelogin'),
