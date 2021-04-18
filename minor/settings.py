c0e82c0b3381369316993f24da5abd8ecd53bf95
Salamander <rohitroy741760@gmail.com>
2021-04-18 15:12:57 +0530

    Migrated to Postgress

--- minor/settings.py
+++ minor/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'minor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'network',
-#         'USER': 'Rohit@mujnet',
-#         'PASSWORD': 'Alohmora@741760',
-#         'HOST': 'mujnet.postgres.database.azure.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7b6ha0mls96kd',
+        'USER': 'twxwloxphwrtgp',
+        'PASSWORD': 'f08de44fdba2759e58ea0a0282ad7fc40f49b9b2acad978dc495e370ea0ec604',
+        'HOST': 'ec2-3-222-127-167.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
