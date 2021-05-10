af3fa2db8a4f3df70dfabbb07e52bd267f30af07
Salamander <rohitroy741760@gmail.com>
2021-05-10 12:10:46 +0530

    Enchanced Profile Form

--- users/forms.py
+++ users/forms.py
@@ -20,9 +20,24 @@ class UserUpdateForm(forms.ModelForm):
         fields = ['username', 'email']
 
 
+BLOCK_CHOICES = [('G1', 'G1'), ('G2', 'G2'), ('G3', 'G3'), ('B1', 'B1'),
+                 ('B2', 'B2'), ('B3', 'B3'), ('B4', 'B4'), ('B5', 'B5'), ('B6', 'B6'), ('B7', 'B7')]
+
+PROGRAMME_CHOICES = [('B.Tech', 'B.tech'), ('M.Tech', 'M.tech'),
+                     ('BJMC', 'BJMC'), ('BA', 'BA'), ('BSc', 'Bsc')]
+
+BRANCH_CHOICES = [('CSE', 'CSE'), ('IT', 'IT'), ('CCE', 'CCE'), ('ECE', 'ECE'), ('EE', 'EE'), ('Mech', 'Mech'), ('Civil',
+                                                                                                                 'Civil'), ('Chemical', 'Chemical'), ('Economics', 'Economics'), ('English', 'English'), ('Psychology', 'Psycology')]
+
+
 class ProfileUpdateForm(forms.ModelForm):
+    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    bio = forms.CharField(widget=forms.TextInput(attrs={'type': 'text'}))
+    block = forms.CharField(widget=forms.Select(choices=BLOCK_CHOICES))
+    programme = forms.CharField(widget=forms.Select(choices=PROGRAMME_CHOICES))
+    branch = forms.CharField(widget=forms.Select(choices=BRANCH_CHOICES))
 
     class Meta:
         model = Profile
-        fields = ['image', 'bio', 'two_factor',
-                  'city', 'state', 'programme', 'branch']
+        fields = ['image', 'bio', 'two_factor', 'block',
+                  'room', 'programme', 'branch', 'dob']
--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio = models.TextField(default="No Bio")
-    city = models.CharField(default="Jaipur", max_length=100)
-    state = models.CharField(default="Rajasthan", max_length=100)
+    room = models.IntegerField(default=43)
+    block = models.CharField(default="B2", max_length=100)
     programme = models.CharField(default='B.Tech', max_length=15)
     branch = models.CharField(default='CSE', max_length=15)
     face = models.ImageField(blank=True, upload_to='faces')
@@ -22,6 +22,7 @@ class Profile(models.Model):
         User, related_name='following')
     follow_request = models.ManyToManyField(
         User, related_name='requests')
+    dob = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
